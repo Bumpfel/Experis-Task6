3567c1bbb35007ae805262c54244e7ce33ed7bd6
Bumpfel <34800245+Bumpfel@users.noreply.github.com>
2020-10-21 15:01:09 +0200

    chore: initial project setup

---
+++ src/main/java/se/experis/task6herokunate/Task6HerokunateApplication.java
@@ -0,0 +1,13 @@
+package se.experis.task6herokunate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task6HerokunateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task6HerokunateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/experis/task6herokunate/Task6HerokunateApplicationTests.java
@@ -0,0 +1,13 @@
+package se.experis.task6herokunate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task6HerokunateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
